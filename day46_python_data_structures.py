738aa8dec0671824d3485969ed3b173179333a7d
AncyDataScience <selva2711992@gmail.com>
2026-07-28 11:26:30 +0530

    Create day46_python_data_structures.py

---
+++ day46_python_data_structures.py
@@ -0,0 +1,26 @@
+# Task 1: Create a List (Don't forget the '=' sign!)
+aircraft_fleet = ["A350", "A380", "B777"]
+print(aircraft_fleet)
+
+# Task 2: Indexing (Zero-based)
+print(aircraft_fleet[0])
+
+# Task 3: Append to a List
+aircraft_fleet.append("B787")
+print(aircraft_fleet) # Just print the variable name to see the whole list
+
+# Task 4: Create a Dictionary
+pilot_data = {"Name": "Karthik", "ID": 7721, "Active": True}
+print(pilot_data)
+
+# Task 5: Read from a Dictionary
+print(pilot_data["Name"])
+
+#OUTPUT
+
+> ['A350', 'A380', 'B777']
+> A350
+> ['A350', 'A380', 'B777', 'B787']
+> {'Name': 'Karthik', 'ID': 7721, 'Active': True}
+> Karthik
+
